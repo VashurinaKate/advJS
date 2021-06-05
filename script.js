a0d0c01420331912a4b09c058e34fb3e4e35fa01
KateVashurina <wasturina.ekatterina@gmail.com>
2021-06-05 22:51:26 +0300

    lesson3

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 class GoodsItem {
-  constructor(title = "No title", price = 100, img = "noImage") {
-    this.title = title;
+  constructor(id_product, product_name = "No title", price = 100, img = "noImage") {
+    this.id_product = id_product;
+    this.product_name = product_name;
     this.price = price;
     this.img = img;
   }
+
   render() {
     const helpLine = `<div class="holder">
       <div class="smallLine smallLine1"></div>
@@ -12,9 +15,9 @@ class GoodsItem {
     return `
     <div class="goods-item">
       <a href="#" class="goods-imgLink"><img src="img/${this.img}.jpeg" class="goods-img" alt="product"></a>
-      <h3>${this.title}</h3>
+      <h3>${this.product_name}</h3>
       <p>${this.price}</p>
-      <button type="button">Add to Cart</button>
+      <button type="button" class="addToCart" id="${this.id_product}">В корзину</button>
       ${helpLine}
     </div>
     `
@@ -24,52 +27,119 @@ class GoodsList {
   constructor() {
     this.goods = [];
   }
-  fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150, img: 'Shirt' },
-      { title: 'Socks', price: 50, img: 'Socks' },
-      { title: 'Jacket', price: 350, img: 'Jacket' },
-      { title: 'Shoes', price: 250, img: 'Shoes' },
-      { price: 130 },
-      // { title: 'Accessories' },
-      // {  },
-    ];
+  async fetchGoods() {
+    const response = await fetch(`${API_URL}/catalogData.json`);
+    const catalogItems = await response.json();
+    this.goods = catalogItems;
+    // this.goods = [
+    //   { id_product: 12, product_name: 'kate', price: 123 },
+    //   { id_product: 23, product_name: 'vania', price: 234 },
+    //   { id_product: 34, product_name: 'alex', price: 345 },
+    //   { id_product: 45, product_name: 'mary', price: 456 }
+    // ]
   }
 
-  /*
-   Метод, который считает суммарную стоимость всех товаров
-   Тут возник вопрос. Если добавить товар без указания его цены, то метод не будет работать,
-   так как цена будет undefined. Но я задавала значение цены по умолчанию = 100,
-   почему так происходит? Порядок вызовов методов меняла, не помогло...
-  */
-  calcTotalPrice() {
-    let totalPrice = 0;
-    this.goods.forEach((good) => {
-      totalPrice += good.price;
-      console.log(good.price);
-    });
-    document.querySelector('.goods-list').insertAdjacentHTML("beforebegin", `<h3>Total Price: ${totalPrice}</h3>`);
+
+  /**
+    Метод осуществляет добавление в корзину выбранных товаров из GoodsList 
+    Убрать отсюда вызовы методов удаления из корзины и подсчета цены не получилось...
+    */
+  addToCart() {
+    const cartList = new CartList();
+    this.goods.forEach(good => {
+      let button = document.getElementById(`${good.id_product}`);
+      button.addEventListener('click', () => {
+        if ( button.innerText === 'В КОРЗИНУ' ) {
+          button.innerText = 'Добавлено';
+          button.style.background = '#8c5241';
+          cartList.cartGoods.push(good);
+          cartList.render(cartList.cartGoods);
+          cartList.calcTotalPrice(cartList.cartGoods);
+          cartList.removeFromCart();
+        }
+      })
+    })
   }
 
   render() {
     let listHTML = '';
     this.goods.forEach((good) => {
-      const goodItem = new GoodsItem(good.title, good.price, good.img);
+      const goodItem = new GoodsItem(good.id_product, good.product_name, good.price, good.img);
       listHTML += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = listHTML;
   }
 }
 
-// Не совсем поняла, это то, что требуется в 1 задании? 
-class CartGoodsList extends GoodsList {}
-class CartGoodsItem extends GoodsItem {}
+class CartItem {
+  constructor(id_product, product_name = "No title", price = 100, img = "noImage") {
+    this.id_product = id_product;
+    this.product_name = product_name;
+    this.price = price;
+    this.img = img;
+  }
+  render() {
+    return `
+    <div class="cart-item">
+      <a href="#" class="cart-imgLink"><img src="img/${this.img}.jpeg" class="cart-img" alt="product"></a>
+      <h3>${this.product_name}</h3>
+      <p>${this.price}</p>
+      <button type="button" data-id="${this.id_product}" class="removeFromCart">Удалить</button>
+    </div>
+    `
+  }
+}
+class CartList {
+  constructor() {
+    this.cartGoods = [];
+  }
 
-const init = () => {
+  calcTotalPrice(objArr) {
+    let totalPrice = 0;
+    objArr.forEach((good) => {
+      totalPrice += good.price;
+    });
+    document.querySelector('.totalPrice').innerText = totalPrice;
+  }
+
+  removeFromCart() {
+    let removeButtons = document.querySelectorAll('.removeFromCart');
+
+    this.cartGoods.forEach((cartGood) => {
+      removeButtons.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+          let addButton = document.getElementById(`${cartGood.id_product}`);
+          if (cartGood.id_product === +event.target.dataset.id) {
+            this.cartGoods.splice(this.cartGoods.indexOf(cartGood), 1);
+            addButton.innerText = 'В корзину';
+            addButton.style.background = '#cba59a';
+          }
+        })
+      })
+    })
+
+    document.getElementById('updateCart').addEventListener('click', () => {
+      this.render(this.cartGoods);
+      this.calcTotalPrice(this.cartGoods);
+    })
+  }
+
+  render(objArray) {
+    let cartListHTML = '';
+
+    objArray.forEach((good) => {
+      const cartItem = new CartItem(good.id_product, good.product_name, good.price, good.img);
+      cartListHTML += cartItem.render();
+    });
+    document.querySelector('.cart-list').innerHTML = cartListHTML;
+  }
+}
+
+const init = async () => {
   const list = new GoodsList();
-  list.fetchGoods();
+  await list.fetchGoods();
   list.render();
-  list.calcTotalPrice();
+  list.addToCart();
 };
 
 window.onload = init;
